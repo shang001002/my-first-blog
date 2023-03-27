6084f08d3f996ccbe31ab2cd760ca9dc54ebd7f3
shang001002@gmail.com <evan8816@163.com>
2023-03-28 02:51:48 +0900

    test7

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@
     #return render(request, 'blog/post_list.html', {})
     
     
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render,redirect, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -19,8 +19,17 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})    
     
 def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})    
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form}) 
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
